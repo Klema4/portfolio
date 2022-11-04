4f82e2110e321a352c3828bca23a3453b96d700e
Adam Klement <45459954+Klema4@users.noreply.github.com>
2022-11-04 23:41:37 +0100

    Updated files, design and fix some issues

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,8 +1,12 @@
 function copyFunction() {
     const copyText = document.getElementById("copyButton");
+    // Add class to button
     copyText.classList.add("btn-success");
+    // Send message into the console
     console.log("Successfully copied Discord tag to clipboard");
+    // Write text from bracket to clipboard
     navigator.clipboard.writeText("Kléma#2152");
+    // Wait one seconds, then remove class from button
     setTimeout(function() {
         copyText.classList.remove("btn-success");
     }, 1000);
